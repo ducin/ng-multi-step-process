3dab046e0736de7ebae88b8a896db0fb04ac7929
Tomasz Ducin <tomasz.ducin@gmail.com>
2016-07-21 11:27:48 +0200

    new buttons for specific responses

--- src/app.js
+++ src/app.js
@@ -4,35 +4,87 @@ var demoApp = angular.module("demoApp", [
   "cgBusy"
 ]);
 
-demoApp.service("PostModel", ["$q", "Restangular", function($q, Restangular){
-  this.fetch = function(start, end){
-    var url = 'https://jsonplaceholder.typicode.com/posts';
+demoApp.constant("baseURL", "https://jsonplaceholder.typicode.com");
+
+demoApp.config(["RestangularProvider", function(RestangularProvider){
+  RestangularProvider.setFullResponse(true);
+}]);
+
+demoApp.service("Status", function(){
+  this.isSuccessful = function(status){
+    return status === 200 || status === 204;
+  };
+
+  this.needsAuthorization = function(status){
+    return status === 207;
+  };
+});
+
+demoApp.service("Async", ["$q", "$timeout", function($q, $timeout){
+  this.delay = function(originalPromise, time){
+    var deferred = $q.defer();
+
+    $timeout(function() {
+      originalPromise.then(function(response){
+        deferred.resolve(response.data);
+      }, function(reason){
+        deferred.resolve(reason);
+      });
+    }, time);
+    return deferred.promise;
+  };
+}]);
+
+demoApp.service("PostModel", ["$q", "Restangular", "Status", "Async", "baseURL", function($q, Restangular, Status, Async, baseURL){
+  this.getCollection = function(start, end){
+    var url = baseURL + '/posts';
     var params = [];
     if (start !== undefined) { params.push('_start=' + start); }
     if (end !== undefined) { params.push('_end=' + end); }
     if (params.length) { url += '?' + params.join('&'); }
-    return Restangular.allUrl('posts', url).getList();
+    return Restangular.allUrl("base", baseURL).all("posts").getList();
   };
 
-  this.fetchDelayed = function(start, end) {
-    var fetchPromise = this.fetch(start, end);
+  this.getCollectionDelayed = function(start, end) {
+    return Async.delay(this.getCollection(start, end), 2000);
+  };
+
+  this.getItem = function(id){
+    var itemPromise = Restangular.allUrl("base", baseURL).one('posts', id).get();
     var deferred = $q.defer();
 
-    setTimeout(function() {
-      fetchPromise.then(function(data){
-        deferred.resolve(data);
-      }, function(reason){
-        deferred.resolve(reason);
-      });
-    }, 2500);
+    itemPromise.then(function(response){
+      if (Status.isSuccessful(response.status)){ // successful case - no additional authorization
+        deferred.resolve(response.data);
+      } else if (Status.needsAuthorization(response.status)) {
+        debugger;
+      } else {
+        alert("Unhandled status: " + response.status);
+      }
+    }, function(reason){
+      deferred.reject(reason);
+    });
 
     return deferred.promise;
-  }
+  };
+}]);
 
+demoApp.controller("callCtrl", ["$scope", "$uibModal", "PostModel", function($scope, $uibModal, PostModel) {
+  $scope.display = '';
+
+  $scope.call = function(id){
+    PostModel.getItem(id).then(function(response){
+      $scope.display = response.plain();
+    });
+  };
+
+  $scope.clear = function () {
+    $scope.display = '';
+  };
 }]);
 
 demoApp.controller("demoCtrl", ["$scope", "$uibModal", "PostModel", function($scope, $uibModal, PostModel){
-  $scope.postsPromise = PostModel.fetchDelayed(0, 9);
+  $scope.postsPromise = PostModel.getCollectionDelayed(0, 9);
   $scope.postsPromise.then(function(response){
     $scope.posts = response.map((el) => { return el.plain(); });
   });
